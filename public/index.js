a6ab17b547723675a0abcac9a0355721d70f2022
mustafa <mostafashahriar598@gmail.com>
2023-09-04 14:33:19 +0600

    not rendering every task after editing

--- public/index.js
+++ public/index.js
@@ -56,11 +56,10 @@ function render() {
             <div class="bg-secondary-subtle p-2 d-flex align-items-center justify-content-between custome_transition childeNo${i}">
                 <span>
                   <input class="form-check-input me-2" type="checkbox" id="defaultCheck1" onchange="taskCompleted(this.checked , ${i})" ${
-      element.completed ? "checked" : ""
-    }>
-                  <span class="text_decoration_line_through">${
-                    element.title
-                  }</span>
+                  element.completed ? "checked" : ""}>
+                  <span class="text_decoration_line_through">
+                    ${element.title}
+                  </span>
                 </span>
                 <span>
                   <button class="btn btn-light me-3=2" onclick="edit(${i});">
@@ -108,7 +107,7 @@ function hand_edit_save_btn(){
   if(edit_input.value !== ""){
     all_tasks.style.opacity = "1";
     tasks[edit_task_index].title = edit_input.value;
-    render();
+    document.querySelector(`.childeNo${edit_task_index} .text_decoration_line_through`).innerHTML = edit_input.value;
     edit_container.style.display = "none";
     updateLocalStorage();
   }
